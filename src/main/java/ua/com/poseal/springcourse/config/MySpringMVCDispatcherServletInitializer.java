9d9d7102c95342484fe08031e5db103bd4393513
Sergiy Poddubka <poseal@gmail.com>
2021-03-12 18:22:06 +0200

    CRUD, REST, DAO

---
+++ src/main/java/ua/com/poseal/springcourse/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package ua.com.poseal.springcourse.config;
+
+import com.sun.istack.internal.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Nullable
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
---
+++ src/main/java/ua/com/poseal/springcourse/dao/PersonDAO.java
@@ -0,0 +1,35 @@
+package ua.com.poseal.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ua.com.poseal.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT = 0;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+
+}
