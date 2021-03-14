8b44db2b57ed2560d6f98d38ebd73034d6f7c8dc
Sergiy Poddubka <poseal@gmail.com>
2021-03-14 12:31:12 +0200

    Added new fields for person

--- src/main/java/ua/com/poseal/springcourse/dao/PersonDAO.java
+++ src/main/java/ua/com/poseal/springcourse/dao/PersonDAO.java
@@ -15,10 +15,10 @@ public class PersonDAO {
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
     }
 
     public List<Person> index() {
@@ -40,6 +40,8 @@ public class PersonDAO {
     public void update(int id, Person updatedPerson) {
         Person personFromDB = show(id);
         personFromDB.setName(updatedPerson.getName());
+        personFromDB.setAge(updatedPerson.getAge());
+        personFromDB.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
